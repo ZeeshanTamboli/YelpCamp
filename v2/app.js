f30a5038513fba0351a07b6a19f14c18aa5ceaa7
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-06 12:29:34 +0530

    Added Cammpground Showpage

--- v2/app.js
+++ v2/app.js
@@ -14,7 +14,8 @@ app.set("view engine", "ejs");
 //SCHEMA SETUP
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 //Compile schema into a model
@@ -22,7 +23,8 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 
 // Campground.create({
 //     name: "Granite Hill",
-//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+//     image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg",
+//     description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"
 // }, function(err, campground) {
 //   if(err) {
 //     console.log(err);
@@ -43,7 +45,7 @@ app.get("/campgrounds", function(req, res) {
     if(err) {
       console.log(err);
     } else {
-      res.render("campgrounds", {campgrounds: allCampgrounds});
+      res.render("index", {campgrounds: allCampgrounds});
     }
   });
 });
@@ -52,10 +54,26 @@ app.get("/campgrounds/new", function(req, res) {
   res.render("new");
 });
 
+
+// SHOW - Shows more info about one campground
+app.get("/campgrounds/:id", function(req, res) {
+  //find the campground with provided id
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if(err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render("show", {campground: foundCampground});
+    }
+  });
+});
+
+
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = {name: name, image: image};
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
   //Create a new campground and save to DB
   Campground.create(newCampground, function(err, newlyCreated) {
     if(err) {
