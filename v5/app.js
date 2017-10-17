e2d328941728937ac7fb0fae4fd4aaeda67c9fbc
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-17 14:26:11 +0530

    Comments Styling 2

--- v5/app.js
+++ v5/app.js
@@ -13,6 +13,7 @@ mongoose.connect("mongodb://localhost:27017/yelp_camp_v3", {
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 seedDB();
 
