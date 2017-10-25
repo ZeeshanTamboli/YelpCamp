ed510cdaae0bd1a6668c8d7d1955985336f966eb
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-10-25 23:29:38 +0530

    Refactoring Routes

---
+++ v7/routes/comments.js
@@ -0,0 +1,38 @@
+// ============================
+//COMMENT ROUTES
+// ============================
+
+app.get("/campgrounds/:id/comments/new", isLoggedIn, function(req, res) {
+  //find by id
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+    } else {
+        res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", isLoggedIn, function(req, res) {
+  //lookup campground using id
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      //create new comment
+      Comment.create(req.body.comment, function(err, comment) {
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+
+      //connect new comment to campground
+      //redirect to campground showpage
+    }
+  });
+});
