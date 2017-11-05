75174f49eed52e2e82db702b672b023d6bcd2cad
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2017-11-05 11:12:59 +0530

    Refactoring

--- v4/app.js
+++ v4/app.js
@@ -2,7 +2,8 @@ var express    = require("express"),
     app        = express(),
     bodyParser = require("body-parser"),
     mongoose   = require("mongoose"),
-    Campground = require("./models/campground");
+    Campground = require("./models/campground"),
+    Comment    = require("./models/comment"),
     seedDB     = require("./seeds");
 
 mongoose.Promise = global.Promise;
@@ -79,6 +80,30 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
       console.log(err);
     } else {
         res.render("comments/new", {campground: campground});
+    }
+  });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  //lookup campground using id
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+      //create new comment
+      Comment.create(req.body.comment, function(err, comment) {
+        if(err) {
+          console.log(err);
+        } else {
+          campground.comments.push(comment);
+          campground.save();
+          res.redirect("/campgrounds/" + campground._id);
+        }
+      });
+
+      //connect new comment to campground
+      //redirect to campground showpage
     }
   });
 });
