7102ff9c0672ad419ed7da82ff6d49dd5674569f
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-13 12:39:02 +0530

    User Associations Comment

---
+++ v8/routes/campgrounds.js
@@ -0,0 +1,53 @@
+var express = require("express");
+var router = express.Router();
+var Campground = require("../models/campground");
+//DISPLAY CAMPGRPUNDS
+router.get("/", function(req, res) {
+  //Get all campgrounds from DB
+  Campground.find({}, function(err, allCampgrounds){
+    if(err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds/index", {campgrounds: allCampgrounds});
+    }
+  });
+});
+
+//CREATE CAMPGROUND
+router.get("/new", function(req, res) {
+  res.render("campgrounds/new");
+});
+
+
+// SHOW - Shows more info about one campground
+router.get("/:id", function(req, res) {
+  //find the campground with provided id
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground) {
+    if(err) {
+      console.log(err);
+    } else {
+      //render show template with that campground
+      res.render("campgrounds/show", {campground: foundCampground});
+    }
+  });
+});
+
+//FORM FOR CREATING NEW CAMPGROUND
+router.post("/", function(req, res) {
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
+  //Create a new campground and save to DB
+  Campground.create(newCampground, function(err, newlyCreated) {
+    if(err) {
+      console.log(err);
+    } else {
+      //redirect back to campgrounds page
+      res.redirect("/campgrounds"); //GET route of "/campgrounds"
+    }
+  });
+});
+
+
+module.exports = router;
