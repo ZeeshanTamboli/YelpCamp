82f43c09117bfd61cd2616ac3b315d55020895a9
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-15 12:35:08 +0530

    Comment Edit and Update

--- v10/routes/campgrounds.js
+++ v10/routes/campgrounds.js
@@ -45,7 +45,7 @@ router.post("/", isLoggedIn, function(req, res) {
   var author = {
     id: req.user._id,
     username: req.user.username
-  }
+  };
   var newCampground = {
     name: name,
     image: image,
