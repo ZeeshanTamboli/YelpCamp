490bdf0771ac84ff38f4c16fb81c6b0bb7318d33
Zeeshan Tamboli <zeeshan.tamboli@gmail.com>
2018-01-15 11:42:12 +0530

    Authorization of Campground Final

--- v10/routes/campgrounds.js
+++ v10/routes/campgrounds.js
@@ -74,7 +74,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res) {
 });
 
 //Update Campground Route
-router.put("/:id", function(req, res) {
+router.put("/:id", checkCampgroundOwnership, function(req, res) {
   //find and update the correct campground
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground) {
     if (err) {
@@ -86,7 +86,7 @@ router.put("/:id", function(req, res) {
 });
 
 //DESTROY CAMPGROUND ROUTE
-router.delete("/:id", function(req, res) {
+router.delete("/:id", checkCampgroundOwnership, function(req, res) {
   Campground.findByIdAndRemove(req.params.id, function(err) {
     if (err) {
       res.redirect("/campgrounds");
